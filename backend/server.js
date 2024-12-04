58b916deb9944a883fea281ed3710d4bcc62b23f
Malique <maliqueauguste@gmail.com>
2024-12-04 18:09:36 -0500

    got connection to db working

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,9 @@
+require('dotenv').config()
+
 const express = require("express")
-
+const mongoose = require("mongoose")
 const app = express()
-
-app.listen(4000, () => {
-    console.log("Server running")
-})
+console.log(process.env.PORT)
 
 app.use((req, res, next) => {
     console.log(req.path, req.method)
@@ -14,3 +13,13 @@ app.use((req, res, next) => {
 app.get("/", (req, res) => {
     res.json({mssg: "Hi"})
 })
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log("Connected to db and listening on port", process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
